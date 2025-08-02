bafba2c2a28a3dc2940bf24d74766244f6ec5c18
Dabney <dabneyrueda@gmail.com>
2025-08-02 15:44:12 -0500

    pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Tercero",                                 # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Dabney R",                         # Tu nombre
+    author_email="dabneyrueda@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Dabneyjoseph/TercerRepo",     # URL del proyecto
+)
